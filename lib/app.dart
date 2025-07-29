a2530d691207eaf710f11a376f2b6d06496bd75d
Flexer3dn <ktiibwasidney@gmail.com>
2025-07-29 22:42:21 +0300

    git

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:event_locator_app/models/event.dart' as home_screen;
-import 'package:event_locator_app/screens/home/event_management_screen.dart' as home_screen;
+import 'package:event_locator_app/screens/home/event_management_screen.dart'
+    as home_screen;
 import 'package:flutter/material.dart';
 import 'package:event_locator_app/screens/auth/auth_wrapper.dart';
 import 'package:event_locator_app/core/theme/app_theme.dart';
@@ -15,8 +16,8 @@ class LocalEventFinderApp extends StatelessWidget {
     return MaterialApp(
       title: 'Local Event Finder',
       theme: AppTheme.lightTheme,
+
       //darkTheme: AppTheme.darkTheme,
-      
       themeMode: ThemeMode.system,
       home: const SplashScreen(),
       //home: const AuthWrapper(),
@@ -24,6 +25,6 @@ class LocalEventFinderApp extends StatelessWidget {
       //   '/login': (context) => const LoginScreen(), // Register the login route
       //   // Add other named routes if needed
       // },
-    );
+    ); //end
   }
 }
